45ee383bb5362f2fa27a3d53b7acd8f586fa7373
Sam Mistele <smmstl0@gmail.com>
2022-01-13 11:17:36 -0700

    Add random start and establish winning score

--- Game.js
+++ Game.js
@@ -1,10 +1,37 @@
 class Game {
-  constructor() {
-    this.playerturn = 0;
-    this.gameBoard = [0,0,0,0,0,0,0,0,0];
+  constructor(player) {
+    this.one = new Player({id: 1, token: 🍨};
+    this.two = new Player({id: 2, token: 🍦};
+    this.currentPlayer = player;
+    this.gameBoard = {
+      one: 0,
+      two: 0,
+      three: 0,
+      four: 0,
+      five: 0,
+      six: 0,
+      seven: 0,
+      eight: 0,
+      nine: 0
+    };
   }
-startGame() {
-  var playerOne = new Player({id: '1', token: 🍦});
-  var playerTwo = new Player({id: '2', token: 🍨});
+  whoStarts() {
+    var goFirst = Math.floor(math.random() * 1);
+    if (goFirst % 2 === 0) {
+      this.currentPlayer = this.one;
+    }
+    if (goFirst % 2 != 0) {
+      this.playerTurn = 2;
+      this.currentPlayer = this.two;
+    }
   }
+winningScore() {
+  var scoreToWin;
+  if (this.currentPlayer === this.one) {
+    scoreToWin = 3;
+  }
+  if (this.currentPlayer === this.two) {
+    scoreToWin = 6;
+  }
+}
 }
--- Player.js
+++ Player.js
@@ -1,5 +1,6 @@
 class Player {
   constructor(player) {
+    this.player = player.player;
     this.id = player.id;
     this.token = player.token;
     this.wins = [];
