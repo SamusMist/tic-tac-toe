4749393aaf7c048c6ceb98d4ed2935f1e3bf583c
Sam Mistele <smmstl0@gmail.com>
2022-01-13 09:49:48 -0700

    Add Game class skeleton

---
+++ Game.js
@@ -0,0 +1,10 @@
+class Game {
+  constructor() {
+    this.playerturn = 0;
+    this.gameBoard = [0,0,0,0,0,0,0,0,0];
+  }
+startGame() {
+  var playerOne = new Player({id: '1', token: 🍦});
+  var playerTwo = new Player({id: '2', token: 🍨});
+  }
+}
