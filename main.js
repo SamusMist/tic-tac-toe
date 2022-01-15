42fe0e7f3c61f5f146fa318816ca098ef5bf27e4
Sam Mistele <smmstl0@gmail.com>
2022-01-14 17:36:35 -0700

    Fix bug with player turn

--- main.js
+++ main.js
@@ -3,17 +3,7 @@ var newGame = new Game();
 
 // var cubes = documzent.querySelectorAll('.game-board');
 var cubes = document.querySelector('#gameBoard');
+var squareOne = document.getElementById('0');
 
+squareOne.addEventListener('click', boardClickHandler);
 cubes.addEventListener('click', boardClickHandler);
-
-
-function boardClickHandler(event) {
-  newGame.playerMove(event.target.id);
-    for (var i = 0; i < newGame.board.length; i++) {
-      if (newGame.board[i] === event.target.id && newGame.currentPlayer === 1) {
-        event.target.id.innerHTML += `<p>🍨</p>`;
-      } else if (newGame.board[i] === event.target.id && newGame.currentPlayer === 2) {
-          event.target.id.innerHTML += `<p>🍦</p>`;
-        }
-      }
-    }
