e5db00ba29fdaa5a188f00c153da01f3823b7462
Sam Mistele <smmstl0@gmail.com>
2022-01-16 14:05:02 -0700

    Add DOM manipulation

--- Game.js
+++ Game.js
@@ -2,24 +2,25 @@ class Game {
   constructor() {
     this.one = new Player(1, '🧚‍♂️');
     this.two = new Player(2, '🧞‍♀️');
-    this.startingPlayer = 1;
+    this.startingPlayer = this.one.token;
     this.currentPlayer = this.startingPlayer;
     this.board = ['', '', '', '', '', '', '', '', ''];
+    this.winner = null;
   }
 
   whoStarts() {
-    if (this.startingPlayer === 1) {
-      this.startingPlayer = 2;
-    } else {
-      this.startingPlayer = 1;
+    if (this.startingPlayer === this.one.token) {
+      this.startingPlayer = this.two.token;
+    } else if (this.startingPlayer === this.two.token) {
+      this.startingPlayer = this.one.token;
       }
     }
 
   changeTurn() {
-    if (this.currentPlayer === this.one.id) {
-      this.currentPlayer = this.two.id;
-    } else if (this.currentPlayer === this.two.id) {
-      this.currentPlayer = this.one.id;
+    if (this.currentPlayer === this.one.token) {
+      this.currentPlayer = this.two.token;
+    } else if (this.currentPlayer === this.two.token) {
+      this.currentPlayer = this.one.token;
     }
   }
 
@@ -41,44 +42,41 @@ checkForWin() {
   } else if (this.board[2] === this.board[4] && this.board[4] === this.board[6] && this.board[2] !== '') {
     this.win()
   }
-  this.draw();
 }
 
   reset() {
     this.board = ['', '', '', '', '', '', '', '', ''];
     this.currentPlayer = this.startingPlayer;
-    this.whoStarts();
-  }
-
-  draw() {
-    if (!this.board.includes('')) {
-      console.log('Draw!')
-      winnerText.innerHTML += `<h2>Draw!</h2>`
+    this.winner = null;
+    for (var i = 0; i < box.length; i++) {
+      box[i].innerHTML = ``;
     }
+    this.whoStarts();
+    this.changeTurn();
   }
 
   win() {
-    if (this.currentPlayer === 1) {
+    if (this.currentPlayer === this.one.token) {
       this.one.wins++;
-      winnerText.innerHTML += `<h2>🧚‍♂️ wins!</h2>`;
+      this.winner = this.one.token;
+      console.log('player 1 wins')
     }
-    if (this.currentPlayer === 2) {
+    if (this.currentPlayer === this.two.token) {
+      console.log('player 2 wins')
       this.two.wins++;
-      winnerText.innerHTML += `<h2>🧞‍♀️ wins!</h2>`;
+      this.winner = this.two.token;
     }
   }
 
   playerMove(position) {
-    var playerOneValue = '🧚‍♂️';
-    var playerTwoValue = '🧞‍♀️';
     if (this.board[position] !== '' || position < 0 || position > 8) {
       return `Try again, bish`;
     }
-    if (this.currentPlayer === 1) {
-      this.board[position] = playerOneValue;
+    if (this.currentPlayer === this.one.token) {
+      this.board[position] = this.one.token;
     }
-    if (this.currentPlayer === 2) {
-      this.board[position] = playerTwoValue;
+    if (this.currentPlayer === this.two.token) {
+      this.board[position] = this.two.token;
     }
     this.checkForWin();
     this.changeTurn();
--- main.js
+++ main.js
@@ -1,29 +1,71 @@
-// ***** Game Instantiation *****
 var newGame = new Game();
 
-// var cubes = documzent.querySelectorAll('.game-board');
 var board = document.querySelector('#gameBoard');
-var winnerText = document.querySelector('.board-area');
-var playerOneWins = document.querySelector('.playerOneWins');
-var playerTwoWins = document.querySelector('.playerTwoWins');
-// var squareOne = document.getElementById('0');
+var playerOneWins = document.querySelector('.player-one-wins');
+var playerTwoWins = document.querySelector('.player-two-wins');
+var turnCounter = document.querySelector('.turn-counter');
+var box = document.querySelectorAll('.box');
 
-// squareOne.addEventListener('click', boardClickHandler);
+
 board.addEventListener('click', boardClickHandler);
 
 
 function boardClickHandler(event) {
   var boxes = document.querySelectorAll('.box');
   for (var i = 0; i < boxes.length; i++) {
-    if (event.target.id === boxes[i].id && newGame.currentPlayer === 1) {
+    if (event.target.id === boxes[i].id && newGame.currentPlayer === newGame.one.token) {
       boxes[i].innerHTML += `
       <h1>${newGame.one.token}</h1>
       `;
-    } else if (event.target.id === boxes[i].id && newGame.currentPlayer === 2) {
+    } else if (event.target.id === boxes[i].id && newGame.currentPlayer === newGame.two.token) {
         boxes[i].innerHTML += `
         <h1>${newGame.two.token}</h1>
         `;
       }
     }
     newGame.playerMove(event.target.id);
+    changeTurn();
+    winCondition();
   }
+
+function winCondition() {
+  console.log('test')
+  if (newGame.winner === newGame.one.token) {
+    playerOneWins.innerHTML = `<p class='player-one-wins'>${newGame.one.wins}</p>`;
+    turnCounter.innerHTML = `<h2 class='turns'>${newGame.one.token} wins!</h2>`;
+    setTimeout(function () {
+      newGame.reset();
+      clear();
+    }, 2000);
+  } else if (newGame.winner === newGame.two.token) {
+    playerTwoWins.innerHTML = `<p class='player-two-wins'>${newGame.two.wins}</p>`;
+    turnCounter.innerHTML = `<h2 class='turns'>${newGame.two.token} wins!</h2>`;
+    setTimeout(function () {
+      newGame.reset();
+      clear();
+    }, 2000);
+  } else {
+    draw();
+  }
+}
+
+
+function changeTurn() {
+  if (newGame.winner === null) {
+    turnCounter.innerHTML = `<h2 class='turns'>It's ${newGame.currentPlayer} turn!</h2>`;
+}
+}
+
+function clear() {
+  turnCounter.innerHTML = `<h2 class='turns'>It's ${newGame.startingPlayer} turn!</h2>`;
+}
+
+function draw() {
+  if (!newGame.board.includes('') && !newGame.winner) {
+    turnCounter.innerHTML = `<h2 class='turns'>Draw!</h2>`
+    setTimeout(function () {
+      newGame.reset();
+      clear();
+    }, 2000);
+  }
+}
